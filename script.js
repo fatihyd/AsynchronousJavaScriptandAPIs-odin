ac92ed22a3740b75afd8c36c8cd574a1244edba2
Fatih <yildizafatih@gmail.com>
2024-01-09 17:28:08 +0100

    test files added

---
+++ script.js
@@ -0,0 +1,198 @@
+/*
+let stocks = {
+    fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holders: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
+};
+
+let isShopOpen = true;
+
+function order(time, work) {
+    return new Promise((resolve, reject) => {
+        if (isShopOpen) {
+            setTimeout(() => {
+                resolve(work());
+            }, time);
+        } else {
+            reject(console.log("Our shop is closed!"));
+        }
+    });
+}
+
+order(2000, () => {
+    console.log("FRUIT " + stocks.fruits[0] + " was selected...");
+})
+
+    .then(() => {
+        return order(0, () => {
+            console.log("Production has started");
+        });
+    })
+
+    .then(() => {
+        return order(2000, () => {
+            console.log("The " + stocks.fruits[0] + "was chopped...");
+        });
+    })
+
+    .then(() => {
+        return order(1000, () => {
+            console.log("LIQUIDS " + stocks.liquid[0] + " and " + stocks.liquid[1] + " was selected...");
+        });
+    })
+
+    .then(() => {
+        return order(1000, () => {
+            console.log("Started the machine");
+        });
+    })
+
+    .then(() => {
+        return order(2000, () => {
+            console.log("Ice cream was placed on " + stocks.holders[0]);
+        });
+    })
+
+    .then(() => {
+        return order(3000, () => {
+            console.log("TOPPING " + stocks.toppings[0] + " was selected");
+        });
+    })
+
+    .then(() => {
+        return order(1000, () => {
+            console.log("Ice cream is served!!!");
+        });
+    })
+
+    .catch(() => {
+        console.log("The shop is closed for today :(");
+    })
+
+    .finally(() => {
+        console.log("Day ended, shop is closed...");
+    });
+*/
+
+/*
+console.log("Program started...");
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(
+            {
+                data: "Hello, friend!",
+                error: null
+            }
+        );
+    }, 5000);
+});
+
+console.log(myPromise);
+
+console.log("Program in progress...");
+
+myPromise
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("First promise chain complete!");
+            }, 2000);
+        });
+    })
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Second promise chain complete!");
+            }, 10000);
+        });
+    })
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+    });
+*/
+
+/*
+console.log("Program started...");
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(
+            {
+                data: "Hello, friend!",
+                error: null
+            }
+        );
+    }, 5000);
+});
+
+console.log(myPromise);
+
+console.log("Program in progress...");
+
+myPromise
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("First promise chain complete!");
+            }, 2000);
+        });
+    })
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+    });
+
+myPromise
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Second promise chain complete!");
+            }, 4000);
+        });
+    })
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+    });
+*/
+
+/*
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 3000);
+});
+
+const yourPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(20);
+    }, 5000);
+});
+
+Promise.all([myPromise, yourPromise])
+    .then((resolvedValues) => {
+        console.log(resolvedValues[0] + resolvedValues[1]);
+    });
+*/
+
+/*
+// URL (required), options (optional)
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=wrh9poinocYAdSumfg2ofHs8ZBi25GQR&s=cats', {
+    mode: 'cors'
+})
+    .then(function (response) {
+        // Successful response :)
+        console.log("nice");
+    })
+    .catch(function (err) {
+        // Error :(
+        console.log("bad");
+    });
+*/
