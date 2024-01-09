4bca6be24ac6646beec01e94ff842ae399fa2f3a
Fatih <yildizafatih@gmail.com>
2024-01-09 21:34:50 +0100

    refactored  code to use async await instead of regular old promises

--- script.js
+++ script.js
@@ -196,3 +196,50 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=wrh9poinocYAdSumfg2ofHs8Z
         console.log("bad");
     });
 */
+/*
+const server = {
+    people: [
+        {
+            name: "Odin",
+            age: 20,
+        },
+        {
+            name: "Thor",
+            age: 35,
+        },
+        {
+            name: "Freyja",
+            age: 29,
+        },
+    ],
+
+    getPeople() {
+        return new Promise((resolve, reject) => {
+            // Simulating a delayed network call to the server
+            setTimeout(() => {
+                resolve(this.people);
+            }, 2000);
+        });
+    },
+};
+
+function getPersonsInfo(name) {
+    return server.getPeople()
+        .then((people) => {
+            return people.find((person) => {
+                return person.name === name
+            });
+        });
+}
+
+async function getPersonsInfo(name) {
+    const people = await server.getPeople();
+    const person = people.find(person => { return person.name === name });
+    return person;
+}
+
+getPersonsInfo("Odin")
+    .then((val) => {
+        console.log(val.age);
+    });
+*/
