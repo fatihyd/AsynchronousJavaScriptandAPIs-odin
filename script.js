e74b2a94c330f4032d5f642214726231ff0b3a72
Fatih <yildizafatih@gmail.com>
2024-01-11 08:35:06 +0100

    async await exercises added

--- script.js
+++ script.js
@@ -243,3 +243,156 @@ getPersonsInfo("Odin")
         console.log(val.age);
     });
 */
+/*
+console.log("Program starting...");
+
+function fetchPokemon() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            //resolve({name: 'Pickachu', power: 20});
+            
+            reject(new Error("Danger, danger!"));
+        }, 2000);
+    });
+}
+
+async function summonPokemon() {
+    try {
+        let pokemon = await fetchPokemon();
+        console.log(pokemon.name);
+        console.log(pokemon.power);
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    return "Program complete!";
+}
+
+summonPokemon()
+    .then((resolvedVal) => {
+        console.log(resolvedVal);
+    });
+*/
+/*
+console.log("Program starting...");
+
+function fetchUser() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ user: 'Monkey', admin: true });
+        }, 3000);
+    });
+}
+
+function login(obj) {
+    if (obj.admin !== undefined && obj.admin) {
+        console.log("Successfully logged in!");
+    } else {
+        console.log("Failed to log in. Try again!");
+    }
+}
+
+async function summonUser() {
+    let obj = await fetchUser();
+    login(obj);
+
+    return "Program complete!";
+}
+
+summonUser()
+    .then((resolvedVal) => {
+        console.log(resolvedVal);
+    });
+*/
+
+/*
+// the two promises do NOT rely on each other,
+// therefore we can use Promise.all to speed it up
+console.log("Program starting...");
+
+function fetchFast() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Fast done!");
+        }, 2000);
+    });
+}
+
+function fetchSlow() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Slow done!");
+        }, 6000);
+    });
+}
+
+let firstTimestamp = new Date();
+
+async function getThemAll() {
+
+    // let fastResult = await fetchFast();
+    // console.log(fastResult);
+    // let slowResult = await fetchSlow();
+    // console.log(slowResult);
+
+
+    let allResult = await Promise.all([fetchFast(), fetchSlow()]);
+    console.log(allResult[0]);
+    console.log(allResult[1]);
+
+    let secondTimestamp = new Date();
+    let timeElapsed = secondTimestamp - firstTimestamp;
+    return ("Program complete!", timeElapsed);
+}
+
+getThemAll()
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+    });
+*/
+
+/*
+// the two promises rely on eacher other,
+// therefore we can not use Promise.all
+console.log("Program starting...");
+
+function goGetCandy() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                candy: 'sour keys',
+                quantity: 10
+            });
+        }, 2000);
+    });
+}
+
+function sellCandy(candy) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let pricePerCandy = 25;
+            let totalPrice = pricePerCandy * candy.quantity;
+            resolve(totalPrice);
+        }, 3000);
+    });
+}
+
+async function candyShopping() {
+    let candy = await goGetCandy();
+    let totalMoneyMade = await sellCandy(candy);
+
+    console.log("Total money made: " + totalMoneyMade);
+}
+
+// function candyShopping() {
+//     goGetCandy()
+//         .then((candy) => {
+//             sellCandy(candy)
+//                 .then((totalMoneyMade) => {
+//                     console.log("Total money made: " + totalMoneyMade);
+//                 });
+//         });
+// }
+
+candyShopping();
+*/
